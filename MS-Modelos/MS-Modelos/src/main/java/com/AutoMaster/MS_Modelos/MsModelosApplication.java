88a0c3f0f4e26519c99805aebb943d54230013d1
Cristofer Figueroa <cristo.figueroam@duocuc.cl>
2026-05-11 21:44:55 -0400

    Add files via upload

---
+++ MS-Modelos/MS-Modelos/src/main/java/com/AutoMaster/MS_Modelos/MsModelosApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Modelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsModelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsModelosApplication.class, args);
+	}
+
+}
